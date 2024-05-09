fa8178fc597068f5c8cc98dede34bc7752d08959
jsalicetti <jessicasalicetti@yahoo.com>
2024-05-09 14:58:05 -0400

    Identified cost radius and PI

---
+++ app.js
@@ -0,0 +1,11 @@
+// radius is 5 meters
+// PI = 3.1415;
+
+
+const radius = 5
+const PI = 3.1415
+const area = 3.1415 * 5 * 5
+
+// minimum space of 0.8 sq meters
+// area is starting with 20 plants
+// double in number every week
