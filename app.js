f5686c8063a8c0860f92d4769919220ed056d89a
jsalicetti <jessicasalicetti@yahoo.com>
2024-05-09 15:35:46 -0400

    added week doubling growth info

--- app.js
+++ app.js
@@ -5,15 +5,22 @@
 const radius = 5
 const PI = 3.1415
 const area = 3.1415 * 5 * 5
-console.log(area)
+console.log(area);
 
 // each plant requires a minimum space of 0.8
-const space = 0.8
+const minspace = 0.8
 
-// starting with 20 plants and double in number every week
-const plants = 20
+// starting with 20 plants 
+const plant = 20
+const equation = 20/0.8
+console.log(equation)
+
+// the plants double every year
+const week1 = 20
+const week2 = week1*2
+const week3 = week2*2
 
 
 
 
-// 
+
