8071ed37058a2fe87244153cf3e0478a9a4bcc64
jsalicetti <jessicasalicetti@yahoo.com>
2024-05-10 15:15:06 -0400

    calculating the number of plants after a few weeks

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const area = 3.1415 * 5 * 5
 console.log(area);
 
 // each plant requires a minimum space of 0.8
-const minspace = 0.8
+const minSpace = 0.8;
 
 // starting with 20 plants 
 const plant = 20
@@ -19,8 +19,15 @@ console.log(equation)
 const week1 = 20
 const week2 = week1*2
 const week3 = week2*2
+console.log(week1)
+console.log(week2)
+console.log(week3)
 
+// Estimate the plant growth
 
+const capacity = area / minSpace;
+function plantGrowth(weeks) {
 
-
-
+  // Calculate the number of plants after a few weeks (3)
+  console.log();
+  const plantCount = plant * Math.pow(2, weeks);
